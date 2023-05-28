543ba515cd126042776451ea0e0c0e7fa54476aa
Waseem Akram <66058079+evildevill@users.noreply.github.com>
2023-05-28 15:19:52 +0500

    Create Solution.java

---
+++ I Love Lance & Janice/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static String solution(String s) {
+        int l = s.length();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < l; i++) {
+            char c = chars[i];
+            if (c >= 'a' && c <= 'z') {
+                // Calculate the position of the character in the alphabet
+                int pos = c - 'a';
+                // Invert the position to obtain the corresponding character
+                chars[i] = (char) ('a' + 25 - pos);
+            }
+        }
+        return String.valueOf(chars);
+    }
+    
+    public static void main(String[] args) {
+        String s1 = "wrw blf hvv ozhg mrtsg'h vkrhlwv?";
+        System.out.println(Solution.solution(s1));
+
+        String s2 = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!";
+        System.out.println(Solution.solution(s2));
+    }
+}
